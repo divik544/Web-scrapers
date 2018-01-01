8c43300441a6c3bb72e1c1d90bc3b8699ae5e0b4
Divik Mittal <33899376+divik544@users.noreply.github.com>
2018-01-01 09:24:58 +0530

    Added Comments

--- Bitcoin_price_fetcher.py
+++ Bitcoin_price_fetcher.py
@@ -1,14 +1,15 @@
+#Script that fetches bitcoin prices, requests module is used to create a http request and data is fetched in the form of JSON.
 import requests
 
-res=requests.get('https://www.zebapi.com/api/v1/market/ticker/btc/inr')
+res = requests.get('https://www.zebapi.com/api/v1/market/ticker/btc/inr')
 
-data=res.json()
+data = res.json()
 print("Prices in INR:" )
-print("Buy at:",data['buy'])
-print("Sell at :",data['sell'])
+print("Buy at:", data['buy'])
+print("Sell at :", data['sell'])
 
-res=requests.get('https://www.zebapi.com/api/v1/market/ticker/btc/usd')
-data=res.json()
+res = requests.get('https://www.zebapi.com/api/v1/market/ticker/btc/usd')
+data = res.json()
 print("Prices in USD")
-print("Buy at: ",data['buy'])
-print("Sell at :",data['sell'])
+print("Buy at: ", data['buy'])
+print("Sell at :", data['sell'])
