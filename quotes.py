3fd7fd6dc58530b91410b1a0b969579369211d01
Divik Mittal <33899376+divik544@users.noreply.github.com>
2018-01-01 22:33:26 +0530

    added comments

--- quotes.py
+++ quotes.py
@@ -1,18 +1,21 @@
+#scripts to fetch quotes from quotes.toscrape.com
+
 import requests
 from bs4 import BeautifulSoup
-def print_quote(urltext):
-	res=requests.get(urltext)
 
-	soup=BeautifulSoup(res.text,'lxml')
-	# print(soup.prettify())
+def print_quote(urltext):
+	res = requests.get(urltext)	#create a HTTP Request
+
+	soup = BeautifulSoup(res.text,'lxml')
+	
 	for quote in soup.find_all('div', {'class':"quote"}):
-		print(quote.span.text)
-		print("by", quote.small.text)
+		print(quote.span.text)	#quote
+		print("by", quote.small.text)	#author's name
 
 if __name__ == '__main__':
 	iniurl='http://quotes.toscrape.com/'
 	print_quote(iniurl)
 	ct=2
-	while input("Want More?(Y/N)")=='Y':
+	while input("Want More?(Y/N)") == 'Y':
 		print_quote(iniurl+'page/'+str(ct)+'/')
-		ct+=1
+		ct += 1
